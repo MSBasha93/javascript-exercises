c37899cb357807eaac789bc9ffdd5b88ada1bfbd
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-11-14 07:00:54 +0200

    Finish Excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...elementsToRemove) {
+
+    for (let i = 0; i < elementsToRemove.length; i++) {
+        if (array.includes(elementsToRemove[i])) {
+            array = array.filter(x => x !== elementsToRemove[i]);
+        }
+
+    }
+    return array;
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
