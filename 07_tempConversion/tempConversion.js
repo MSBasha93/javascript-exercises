895a97e7267b23d165af591652700c62424e9155
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-11-15 06:09:46 +0200

    Update Exercise 07
    test passed 2 of 6 tests because it fails negative values
    and doesn't round to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  return Math.round((tempF - 32) * 5/9 , 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return Math.round(tempC * 9/5 +32 , 1);
 };
 
 // Do not edit below this line
