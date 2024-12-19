b46e3cb649a4fa3fb8a78fcd4adacadd2d8c8c1a
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-12-19 18:24:12 +0200

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+    
+    const reversedString = cleanedString.split('').reverse().join('');
+    
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
