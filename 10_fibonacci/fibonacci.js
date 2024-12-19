582d231cd10aff559cfd9365440ab1cc1509103c
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-12-19 18:32:05 +0200

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0) return "OOPS";
+    let arr =[0,1,1];
+    for (let i=3;i<=n;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
 };
 
 // Do not edit below this line
