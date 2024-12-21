eeea3c013f396b9047aa93b8b0dc1d200e1f2249
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-12-21 10:27:57 +0200

    Finish 12_findTheOldest
    I was in a dumb error of confusing 'name' to be
    arr[0].name instead of being the whole person obj!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const todayDate = new Date().getFullYear();
+    
+    arr.sort((a,b) => {
+        const ageA = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth : todayDate - a.yearOfBirth;
+        const ageB = b.yearOfDeath ? b.yearOfDeath - b.yearOfBirth : todayDate - b.yearOfBirth;
+        return ageB - ageA;
+    });
+    
+    return arr[0]; // Return the entire person object
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
